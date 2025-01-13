83c946537cd1b303636ab5f5b68ca32ffe582142
MorRieri <eilara@yandex.ru>
2025-01-13 18:18:44 +0300

    added structure

---
+++ src/templates/components/_ui/accordion/accordion.js
@@ -0,0 +1,21 @@
+// Accordion
+
+let initAccordionItems = (block) => {
+    let accordion_items = block.querySelectorAll(".accordion");
+
+    accordion_items.forEach(item => {
+        let accordion_button = item.querySelector('.accordion--button')
+
+        accordion_button.addEventListener("click", function(e){
+
+            accordion_items.forEach(it => {
+
+                it !== item ? it.classList.remove('is_open') : it.classList.toggle('is_open')
+
+            });
+
+        })
+    })
+}
+
+initAccordionItems(document)
